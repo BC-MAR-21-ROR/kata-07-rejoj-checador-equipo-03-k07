763b686369556756853db0f72dec55a3770d2519
Angelina Moreno <anyelinamoreno@gmail.com>
2021-04-16 17:07:54 +0200

    Add Column with state for employees
    Co-authored-by: EusebioAjas <eusebioajas6@gmail.com>
    Co-authored-by: RobertVel666 <robertvelasco99@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_15_203352) do
+ActiveRecord::Schema.define(version: 2021_04_16_143211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2021_04_15_203352) do
     t.bigint "company_branch_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "state", default: true
     t.index ["company_branch_id"], name: "index_employees_on_company_branch_id"
   end
 
