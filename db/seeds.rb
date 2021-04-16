1f2b629723735e5f039899a98124a1f0e5837a53
Angelina Moreno <anyelinamoreno@gmail.com>
2021-04-16 17:14:43 +0200

    Create some seeds
    Co-authored-by: EusebioAjas <eusebioajas6@gmail.com>
    Co-authored-by: RobertVel666 <robertvelasco99@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,29 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# rails -T db           # => I can see every task for database
+# rails db:seed:replant # =>
+# The difference with db:reset you can execute with server running, because delete registre not database and run seeds
+# rails db:truncate_all # =>
+# The difference with db:destroy you can execute with server running because delete registre not database
+
 Admin.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+3.times do |number|
+  CompanyBranch.create!(
+    name: "Branch#{number}",
+    address: "Address#{number}",
+    admin_id: Admin.first.id
+  )
+end
+
+CompanyBranch.all.each do |company_branch|
+  number = Random.rand(1..100)
+  Employee.create!(
+    name: "Employee#{number}",
+    email: "email#{number}",
+    position: "position#{number}",
+    private_number: number,
+    company_branch_id: company_branch.id
+  )
+end
