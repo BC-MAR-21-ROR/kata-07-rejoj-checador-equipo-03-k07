69cc0b680aeeb2d6f7b4923c279cd8343abea310
Angelina Moreno <anyelinamoreno@gmail.com>
2021-04-19 11:03:14 +0200

    Generate private number to employee

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:admin) do
+    Admin.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+  end
+  let(:company_branch) do
+    CompanyBranch.create!(name:'Foo', address:'Foo', admin_id: admin.id )
+  end
+  describe '#generate_private_number' do
+    context 'Before employee is created' do
+      it 'generate a private number' do
+        employee = Employee.new(company_branch_id: company_branch.id)
+        expect(employee.private_number).to be_nil
+        employee.save!
+        expect(employee.private_number).to be_present
+      end
+    end
+  end
 end
