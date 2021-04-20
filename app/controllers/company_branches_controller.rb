0c1bbb9e50e06cd28989c628bdc73ad5d3d34fd5
EusebioAjas <eusebioajas6@gmail.com>
2021-04-20 12:29:57 -0500

    add crud operations
    Angelina Moreno <anyelinamoreno@gmail.com>
    Co-authored-by: RobertVel666 <robertvelasco99@gmail.com>

--- app/controllers/company_branches_controller.rb
+++ app/controllers/company_branches_controller.rb
@@ -1,6 +1,44 @@
 class CompanyBranchesController < ApplicationController
-  
+  before_action :set_current_branch, only: %i[edit update destroy]
+
   def index
     @company_branches = CompanyBranch.all
+  end
+
+  def new
+    @company_branch = CompanyBranch.new
+  end
+
+  def create
+    @company_branch = CompanyBranch.new(company_params)
+    @company_branch.admin_id = current_admin.id
+    if @company_branch.save
+      redirect_to company_branches_path, notice: 'Successfully created company branch'
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    @company_branch.update(company_params)
+
+    redirect_to company_branches_path, notice: 'Your company branch has been deleted'
+  end
+
+  def destroy
+    @company_branch.destroy
+    redirect_to company_branches_path
+  end
+
+  private
+
+  def set_current_branch
+    @company_branch = CompanyBranch.find(params[:id])
+  end
+  
+  def company_params
+    params.require(:company_branch).permit(:name, :address)
   end
 end
