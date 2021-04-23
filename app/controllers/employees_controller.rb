042008f9736526f62027d9b1b6b076f785e37a46
robert velasco <robertvelasco99@gmail.com>
2021-04-23 14:28:59 -0500

    Created methods for the employee crud
    Co-authored-by: EusebioAjas <eusebioajas6@gmail.com>
    Co-authored-by: Anxhe <anyelinamoreno@gmail.com>
    Co-authored-by: SpidySamurai <javierchiortiz@gmail.com>

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,69 +1,47 @@
 class EmployeesController < ApplicationController
+  before_action :set_current_branch
   before_action :set_employee, only: %i[ show edit update destroy ]
 
-  # GET /employees or /employees.json
+
   def index
-    @employees = Employee.all
+    @employees = @company_branch.employees
   end
 
-  # GET /employees/1 or /employees/1.json
-  def show
-  end
+  def show; end
 
-  # GET /employees/new
   def new
-    @employee = Employee.new
+    @employee = @company_branch.employees.build
   end
 
-  # GET /employees/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /employees or /employees.json
   def create
-    @employee = Employee.new(employee_params)
-
-    respond_to do |format|
-      if @employee.save
-        format.html { redirect_to @employee, notice: "Employee was successfully created." }
-        format.json { render :show, status: :created, location: @employee }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @employee.errors, status: :unprocessable_entity }
-      end
+    @employee = @company_branch.employees.build(employee_params)
+    if @employee.save
+      redirect_to company_branch_employees_path(@company_branch),
+                  notice: 'Your company branch has been created'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /employees/1 or /employees/1.json
   def update
-    respond_to do |format|
-      if @employee.update(employee_params)
-        format.html { redirect_to @employee, notice: "Employee was successfully updated." }
-        format.json { render :show, status: :ok, location: @employee }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @employee.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /employees/1 or /employees/1.json
-  def destroy
-    @employee.destroy
-    respond_to do |format|
-      format.html { redirect_to employees_url, notice: "Employee was successfully destroyed." }
-      format.json { head :no_content }
+    if @employee.update(employee_params)
+      redirect_to company_branch_employees_path(@company_branch),
+                  notice: 'Your company branch has been updated'
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_employee
-      @employee = Employee.find(params[:id])
-    end
+  def employee_params
+    params.require(:employee).permit(:email, :name, :position, :employee_number, :private_number)
+  end
+    
+  def set_employee 
+    @employee = Employee.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def employee_params
-      params.require(:employee).permit(:email, :name, :position, :employee_num, :private_num)
-    end
+  def set_current_branch
+    @company_branch = CompanyBranch.find(params[:company_branch_id])
+  end
 end
