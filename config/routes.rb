85eaca1b75501741e4ce876067c9ca54e6c8877c
robert velasco <robertvelasco99@gmail.com>
2021-04-23 13:35:03 -0500

    Nested routes for accesing branch model and employee
    Co-authored-by: EusebioAjas <eusebioajas6@gmail.com>
    Co-authored-by: Anxhe <anyelinamoreno@gmail.com>
    Co-authored-by: SpidySamurai <javierchiortiz@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   scope :admins do
-    resources :company_branches
+    resources :company_branches do
+      resources :employees
+    end
   end
   root to: 'company_branches#index'
 
