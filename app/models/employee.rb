f91d3185840fd46baaf49de8aaa9d3a858ae43ac
robert velasco <robertvelasco99@gmail.com>
2021-04-23 13:37:11 -0500

    Added random number for employee number
    Co-authored-by: EusebioAjas <eusebioajas6@gmail.com>
    Co-authored-by: Anxhe <anyelinamoreno@gmail.com>
    Co-authored-by: SpidySamurai <javierchiortiz@gmail.com>

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,7 +4,7 @@ class Employee < ApplicationRecord
   belongs_to :company_branch
   has_many :logs
 
-  before_create :generate_private_number
+  before_create :generate_private_number, :generate_employee_number
 
   validates :private_number, uniqueness: true
 
@@ -13,4 +13,9 @@ class Employee < ApplicationRecord
   def generate_private_number
     self.private_number = SecureRandom.hex(3).upcase
   end
+
+  def generate_employee_number
+    self.employee_number = rand(100..1000)
+  end
+
 end
