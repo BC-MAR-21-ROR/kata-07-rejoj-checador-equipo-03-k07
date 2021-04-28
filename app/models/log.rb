2443a62de30f6b3b03aa632b8e96d223360554e3
Angelina Moreno <anyelinamoreno@gmail.com>
2021-04-28 17:55:57 +0200

    Scope log data:
    - Filter log by day
    - Filter log by month
    - Filter log by company_branch
    - Filter log by employee
    
    Co-authored-by: RobertVel666 <robertvelasco99@gmail.com>
    Co-authored-by: SpidySamurai <javierchiortiz@gmail.com>
    Co-authored-by: EusebioAjas <eusebioajas6@gmail.com>

--- app/models/log.rb
+++ app/models/log.rb
@@ -6,6 +6,13 @@ class Log < ApplicationRecord
   validate :one_check_in_by_day, on: :create
   validate :one_check_out_by_day, on: :update
 
+  scope :by_day, ->(date) { where(check_in: date) }
+  scope :by_month, ->(date) { where(check_in: date.beginning_of_month..date.end_of_month) }
+  scope :by_company_branch, lambda { |company_branch_id|
+    joins(:employee).where(employee: { company_branch_id: company_branch_id })
+  }
+  scope :by_employee, ->(employee_id) { where(employee_id: employee_id) }
+
   def one_check_in_by_day
     range = Time.now.beginning_of_day..Time.now.end_of_day
     error = 'check in already exist'
