18e30c3a4425a9bea16a45ba1e75771d7c821acf
Angelina Moreno <anyelinamoreno@gmail.com>
2021-04-30 00:06:57 +0200

    Clean code smell: move logic from controller to model.
    Co-authored-by: RobertVel666 <robertvelasco99@gmail.com>
    Co-authored-by: SpidySamurai <javierchiortiz@gmail.com>
    Co-authored-by: EusebioAjas <eusebioajas6@gmail.com>

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,17 +2,13 @@
 
 class ReportsController < ApplicationController
   def index
-    @results = attendance
+    @results = Log.attendance(date)
   end
 
   def show
     case params[:report]
-    when 'attendance'
-      @results = attendance
-    when 'average_check'
-      @results = average_check
-    when 'absence'
-      @results = absence
+    when 'attendance', 'average_check', 'absence'
+      @results = Log.send(params[:report], date)
     else
       return render file: %(#{Rails.root}/public/404.html), layout: false, status: 404
     end
@@ -23,31 +19,5 @@ class ReportsController < ApplicationController
 
   def date
     @date ||= params[:date] ? Date.parse(params[:date]) : Date.yesterday
-  end
-
-  def attendance
-    @logs_by_day = Log.by_day(date).map do |log|
-      {
-        employee_id: log.employee.id,
-        employee_name: log.employee.name,
-        company_branch_name: log.employee.company_branch.name,
-        date: date
-      }
-    end
-    @logs_by_day.sort_by! { |log| log[:company_branch_name] }
-  end
-
-  def average_check
-    [
-      {
-        company_branch: %(total : #{CompanyBranch.count}),
-        check_in: Time.at(Log.by_month(date).average_check('check_in')).to_s(:time),
-        check_out: Time.at(Log.by_month(date).average_check('check_out')).to_s(:time)
-      }
-    ]
-  end
-
-  def absence
-    Log.absence_by_month(date).map(&:attributes)
   end
 end
--- app/models/log.rb
+++ app/models/log.rb
@@ -12,12 +12,43 @@ class Log < ApplicationRecord
     joins(:employee).where(employee: { company_branch_id: company_branch_id })
   }
   scope :by_employee, ->(employee_id) { where(employee_id: employee_id) }
-  scope :average_check, ->(check) { average(%(#{check}::time)) }
+  scope :average_time, ->(check) { average(%(#{check}::time)) }
   scope :absence_by_month, lambda { |date|
     by_month(date).joins(:employee).select(
       %(employees.id, employees.name, #{workdays(date)} - count(*) as absence)
     ).group('employees.id')
   }
+
+  def self.attendance(date)
+    @logs_by_day = by_day(date).map do |log|
+      {
+        employee_id: log.employee.id,
+        employee_name: log.employee.name,
+        company_branch_name: log.employee.company_branch.name,
+        date: date
+      }
+    end
+    @logs_by_day.sort_by! { |log| log[:company_branch_name] }
+  end
+
+  def self.average_check(date)
+    [
+      {
+        company_branch: %(total : #{CompanyBranch.count}),
+        check_in: Time.at(by_month(date).average_time('check_in')).to_s(:time),
+        check_out: Time.at(by_month(date).average_time('check_out')).to_s(:time)
+      }
+    ]
+  end
+
+  def self.absence(date)
+    absence_by_month(date).map(&:attributes)
+  end
+
+  def self.workdays(date)
+    (date.beginning_of_month..date.end_of_month).count(&:on_weekday?)
+  end
+
   def one_check_in_by_day
     range = Time.now.beginning_of_day..Time.now.end_of_day
     error = 'check in already exist'
@@ -28,9 +59,5 @@ class Log < ApplicationRecord
     range = Time.now.beginning_of_day..Time.now.end_of_day
     error = 'check out already exist'
     errors.add :base, error if employee.logs.exists?(check_out: range)
-  end
-
-  def self.workdays(date)
-    (date.beginning_of_month..date.end_of_month).count(&:on_weekday?)
   end
 end
