d5ab9500e3e79fa83126989c8c5148e45b300424
Angelina Moreno <anyelinamoreno@gmail.com>
2021-05-01 15:47:20 +0200

    Fix flash messages reports controller
    Co-authored-by: SpidySamurai <javierchiortiz@gmail.com>
    Co-authored-by: EusebioAjas <eusebioajas6@gmail.com>
    Co-authored-by: RobertVel666 <robertvelasco99@gmail.com>

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,14 +3,17 @@
 class ReportsController < ApplicationController
   def index
     @results = Log.attendance(date)
+    flash[:alert] = 'No results for this report' if @results.empty?
   end
 
   def show
     case params[:report]
     when 'attendance', 'average_check', 'absence'
       @results = Log.send(params[:report], date)
-
-      return redirect_to(reports_path, alert: 'No results for this report') if @results.empty?
+      if @results.empty?
+        flash[:alert] = 'No results for this report'
+        return render :index
+      end
 
       respond_to do |format|
         format.xlsx
@@ -18,6 +21,7 @@ class ReportsController < ApplicationController
       end
     else
       return render file: %(#{Rails.root}/public/404.html), layout: false, status: 404
+
     end
   end
 
