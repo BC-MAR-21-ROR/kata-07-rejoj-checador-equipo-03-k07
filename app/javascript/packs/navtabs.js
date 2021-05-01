5215084d5b681e0334447dc234f0cef33ccb59cc
Angelina Moreno <anyelinamoreno@gmail.com>
2021-05-01 15:26:53 +0200

    Adjust function to select tab checking activePath
    Co-authored-by: RobertVel666 <robertvelasco99@gmail.com>
    Co-authored-by: SpidySamurai <javierchiortiz@gmail.com>
    Co-authored-by: EusebioAjas <eusebioajas6@gmail.com>

--- app/javascript/packs/navtabs.js
+++ app/javascript/packs/navtabs.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let path = document.location.pathname.split("/").pop();
-  let active = document.querySelector(`#${path}`) || document.querySelector("#attendance");
+  let path = document.location.pathname.split("/")
+  let activePath = path.find(element => /attendance|absence|average_check/.test(element));
+  let active = document.querySelector(`#${activePath}`) || document.querySelector("#attendance");
   let toggleClass = (element, className) => element.classList.toggle(className);
   active.setAttribute("aria-current", "page");
   toggleClass(active, "active");
