b32fe5bfec8a9cdd9b9b28fd7a8ec01d8f960cdc
Angelina Moreno <anyelinamoreno@gmail.com>
2021-05-01 15:54:36 +0200

    Filter data by date in report:
    - Create _input_date partial to insert date via input.
    - Select date of the input via querySelector.
    - Change path via javascript change event.
    - Adjustment of data in the log model to show the date in report.
    
    Co-authored-by: SpidySamurai <javierchiortiz@gmail.com>
    Co-authored-by: EusebioAjas <eusebioajas6@gmail.com>
    Co-authored-by: RobertVel666 <robertvelasco99@gmail.com>

--- app/models/log.rb
+++ app/models/log.rb
@@ -15,17 +15,17 @@ class Log < ApplicationRecord
   scope :average_time, ->(check) { average(%(#{check}::time)) }
   scope :absence_by_month, lambda { |date|
     by_month(date).joins(:employee).select(
-      %(employees.id, employees.name, #{workdays(date)} - count(*) as absence)
+      %('#{date.strftime('%b %Y')}' AS "Month",employees.id, employees.name, #{workdays(date)} - count(*) as absence)
     ).group('employees.id')
   }
 
   def self.attendance(date)
     @logs_by_day = by_day(date).map do |log|
       {
+        date: date,
         employee_id: log.employee.id,
         employee_name: log.employee.name,
-        company_branch_name: log.employee.company_branch.name,
-        date: date
+        company_branch_name: log.employee.company_branch.name
       }
     end
     @logs_by_day.sort_by! { |log| log[:company_branch_name] }
@@ -34,6 +34,7 @@ class Log < ApplicationRecord
   def self.average_check(date)
     [
       {
+        month: date.strftime('%b %Y'),
         company_branch: %(total : #{CompanyBranch.count}),
         check_in: Time.at(by_month(date).average_time('check_in')).to_s(:time),
         check_out: Time.at(by_month(date).average_time('check_out')).to_s(:time)
