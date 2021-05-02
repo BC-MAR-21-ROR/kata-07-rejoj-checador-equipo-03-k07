a1ecdc8dc5d857d7752089219056775e8b31557f
Angelina Moreno <anyelinamoreno@gmail.com>
2021-05-02 14:16:20 +0200

    Install and config popper.js
    Co-authored-by: EusebioAjas <eusebioajas6@gmail.com>
    Co-authored-by: RobertVel666 <robertvelasco99@gmail.com>
    Co-authored-by: SpidySamurai <javierchiortiz@gmail.com>

--- app/javascript/src/bootstrap.js
+++ app/javascript/src/bootstrap.js
@@ -1,3 +1,13 @@
 // inside app/javascript/bootstrap.js
 
+import * as bootstrap from "bootstrap"
 import "bootstrap/js/src/alert";
+import "bootstrap/js/src/tooltip";
+
+document.addEventListener("DOMContentLoaded", function () {
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+});
+
